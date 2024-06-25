f0a9e7e45f8ffe7b80c040143219d90b1176d519
Albert2438 <88531176+Albert2438@users.noreply.github.com>
2024-06-25 23:58:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 
-const img =document.getElementById("img");
-const select =document.getElementById("select");
-const tabs = document.getElementById("tabs");
-const search =document.getElementById("search");
+//const img =document.getElementById("img");
+const search = document.getElementById("search");
 const Anime = document.getElementById("Anime");
 
 
 
  
-
-img.addEventListener('click',() =>{
+/*
+//img.addEventListener('click',() =>{
     tabs.style.display = 'block';
-})
-tabs.addEventListener('blur',() =>{
-    tabs.style.display = 'none';
-})
-//document.addEventListener('click',(event)=>{
-   // if(event.target !== img && event.target !== tabs){
-    //    tabs.style.display = 'none';
-  //  }
-// })
+
+  */
 search.addEventListener ('keypress',(event)=>{
     if(event.key ==='Enter'){
-        window.location.href= "Anime.html";
+       window.location.href="Anime.html";
     }
 })
+
 
  
