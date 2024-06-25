820b64e353d375cffaaadd900fc721270f7cc435
Albert2438 <88531176+Albert2438@users.noreply.github.com>
2024-06-25 21:29:19 +0100

    Update index.js
    Fixed search bar

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const img =document.getElementById("img");
 const select =document.getElementById("select");
 const tabs = document.getElementById("tabs");
+const search =document.getElementById("search");
+const Anime = document.getElementById("Anime");
 
+
+
+ 
 
 img.addEventListener('click',() =>{
     tabs.style.display = 'block';
@@ -10,10 +15,15 @@ img.addEventListener('click',() =>{
 tabs.addEventListener('blur',() =>{
     tabs.style.display = 'none';
 })
-document.addEventListener('click',(event)=>{
-    if(event.target !== img && event.target !== tabs){
-        select.style.display = 'none';
+//document.addEventListener('click',(event)=>{
+   // if(event.target !== img && event.target !== tabs){
+    //    tabs.style.display = 'none';
+  //  }
+// })
+search.addEventListener ('keypress',(event)=>{
+    if(event.key ==='Enter'){
+        window.location.href=Anime.html;
     }
-}
-    
-)
+})
+
+ 
